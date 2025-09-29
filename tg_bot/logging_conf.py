f00f6c4e58e863ff7bf7cf38a99c745ff30cda02
Sbitnev <sashasbitnev2002@gmail.com>
2025-09-29 12:18:59 +0300

    formatted

--- tg_bot/logging_conf.py
+++ tg_bot/logging_conf.py
@@ -12,21 +12,22 @@ rotating_handler = RotatingFileHandler(
     os.path.join(log_dir, "service.log"),
     encoding="utf-8",
     maxBytes=10 * 1024 * 1024,  # Максимум 10MB на файл
-    backupCount=10  # До 10 резервных копий
+    backupCount=10,  # До 10 резервных копий
 )
 
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
-console_handler.stream = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+)
+console_handler.stream = open(
+    sys.stdout.fileno(), mode="w", encoding="utf-8", buffering=1
+)
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        rotating_handler,
-        console_handler
-    ]
+    handlers=[rotating_handler, console_handler],
 )
 
 logger = logging.getLogger(__name__)
